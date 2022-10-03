81990c26bf9d6d9230d0eac636c6310ddf303a26
khushi-2002 <khushiagarwal915@gmail.com>
2022-10-03 13:52:45 +0530

    Updated

--- crud_database_app/contacts.py
+++ crud_database_app/contacts.py
@@ -12,6 +12,11 @@ class contact:
         self.root=root # Stores the container of my application and can be accessible from anywhere
         self.create_left_logo() # calling the function for creating the logo
         self.create_label_frame() #calling the function for creating the frame
+        self.create_message_area()
+        self.create_tree_view()
+        ttk.style = ttk.Style()
+        ttk.style.configure("Treeview", font=('helvetica',10))
+        ttk.style.configure("Treeview.Heading", font=('helvetica',12, 'bold'))
     
     # For displaying the logo
     def create_left_logo(self):
@@ -36,6 +41,29 @@ class contact:
         self.numfield.grid(row=3,column=2,sticky=W,padx=5,pady=2)
         Button(labelFrame, text="Add Contact", command="", bg="blue",fg="white").grid(row=4, column=2,sticky=E, padx=5, pady=5)
         
+    # For displaying the message after performing any operations on the data
+    
+    def create_message_area(self):
+        self.message= Label(text="", fg="red")
+        self.message.grid(row=3,column=1, sticky=W)
+    
+    
+    # For creating the tree view for displaying the data
+    
+    def create_tree_view(self):
+        self.tree= ttk.Treeview(height=10, columns=("email", "number")) # ttk also having widgets like button
+        self.tree.grid(row=6, column=0, columnspan=3) #position
+        self.tree.heading('#0', text='Name', anchor=W) 
+        self.tree.heading('email', text='Email Address', anchor=W)
+        self.tree.heading('number', text='Contact Number', anchor=W)
+        
+    
+    # For creating the scroll bar
+    def create_scrollbar(self):
+        self.scrollbar= Scrollbar(orient='vertical', command=self.tree.yview)
+        self.scrollbar.grid(row=6, column=3, rowspan=10, sticky="sn")
+            
+                
  # First parameter defines the position it gonna be placed!       
 if __name__ == '__main__': # starts the execution from here and executes the code underneth
     root = Tk() # creates an application window
